b4d1f2810d5e0b9788bafc1c79c854b633e1538b
keethu-180 <kkeerthanakeethu@gmail.com>
2019-12-01 23:45:48 -0800

    updated settings file

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pxe9bat@ywfmpl$zk0_!fx#5kzfum%&+b_i+9va)cj%)d^4_f5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['keerthana-app.herokuapp.com']
 
 
 # Application definition
